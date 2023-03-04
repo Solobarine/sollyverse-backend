98417370111c5868bf98875ff3c5b9a69e6b2e99
NAME <mail@gmail.com>
2023-03-04 20:14:12 +0100

    Create Like Controller

---
+++ src/controllers/likeController.js
@@ -0,0 +1,41 @@
+const Like = require('../models/Like');
+const schema = require ('./validate/like');
+
+module.exports = {
+  create: async (req, res) => {
+    // Validate like
+    const validate = schema.validate(req.body)
+    if (validate.error) return res.status(400).send({error: validate.error})
+
+    // Check if like exists
+    const checkLike = await Like.find({email: req.body.email, destinationId: req.body.destinationId})
+    if (checkLike) return res.status(400).send('You already liked this destination')
+
+    // save likes
+    const like = new Like(req.body)
+    await like.save()
+  },
+  delete: async (req, res) => {
+    // Check if like exists
+    const checkLike = await Like.find({email: req.body.email, destinationId: req.body.destinationId})
+    if (!checkLike) return res.status(400).send('Unable to unlike')
+
+    // Remove like
+    await Like.deleteOne({email: req.body.email, destinationId: req.body.destinationId})
+  },
+  showNumberOfLikes: async (id) => {
+    const numberOfLikes = await Like.aggregate().count(id)
+    if (!numberOfLikes) return null
+    
+    return numberOfLikes
+  },
+  mostPopularDestinations: async () => {
+    const popularDestinations = await Like.aggregate().sortByCount('destinationId').limit(5)
+    return popularDestinations
+  },
+  favouriteDestinations: async (email) => {
+    const favourites = await Like.find({email}).select('destinationId')
+    if (!favourites) return null
+    return favourites
+  }
+}
