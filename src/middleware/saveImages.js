22447e6d3a97bcc1143614f9a8b3240bde8f1a1e
Solomon <solobarine@gmail.com>
2023-04-08 03:43:48 +0100

    Add save image to database functionality

--- src/middleware/saveImages.js
+++ src/middleware/saveImages.js
@@ -1,12 +1,19 @@
-const multer = require ('multer')
+const fs = require ('fs')
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, `public/countries/${country}/${city}`)
-  },
-  filename: (req, file, callback) => {
-    callback(null, file.originalname)
-  }
-})
+const saveImages = (images, country, city) => {
+  let directory = []
+  images.map((image, index) => {
+    const filePath = `./public/countries/${country}/${city}`
+    const imagePath = `/coutries/${country}/${city}${index}`
+    fs.mkdir(filePath,{recursive: true},(err) => {if (err) console.log(err)})
+    fs.writeFile( `./public/countries/${country}/${city}${index}`, image, function (err) {
+      if (err) {
+        console.log(err)
+      }
+    })
+    return directory.push(imagePath)
+  })
+  return directory
+}
 
-module.exports = storage
+module.exports = saveImages
--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,6 +1,6 @@
-const multer = require ('multer')
-const storage = require('./saveImages')
+// const multer = require ('multer')
+// const storage = require('./saveImages')
 
-const upload = multer({storage: storage, limits: {fileSize: 2500009}})
+// const upload = multer({storage: storage, limits: {fileSize: 2500009}})
 
-module.exports = upload
+// module.exports = upload
