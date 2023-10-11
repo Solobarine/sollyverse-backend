ec1328d4fb767a1426678783a6e0b5e33da70143
Solly <solobarine@gmail.com>
2023-10-11 08:47:06 +0100

    Modify Server methods

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require ('express');
 const cors = require ('cors');
-const cookie = require ('cookie');
 const mongoose = require ('mongoose');
-const path = require ('path')
  
 const user = require ('./src/routes/User')
 const city = require ('./src/routes/City')
@@ -11,25 +9,24 @@ const like = require ('./src/routes/Like')
 const message = require ('./src/routes/Message')
 const reservation = require('./src/routes/Reservation')
 const review = require ('./src/routes/Review');
-const {json} = require('express');
 
 
 require('dotenv').config();
 
 //const password = process.env.SECURE_PASSWORD;
 
-//const mongo_url = `mongodb+srv://sollyverse:${password}@sollyverse.qr8xevw.mongodb.net/?retryWrites=true&w=majority`
-const mongo_url = `mongodb://localhost:27017/`
-const whitelist = [process.env.DEVELOPMENT_ADDRESS, process.env.PRODUCTION_ADDRESS]
-const corsOption = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
+const mongo_url = process.env.MONGODB_URL_DEV
+
+// const whitelist = [process.env.DEVELOPMENT_ADDRESS, process.env.PRODUCTION_ADDRESS]
+// const corsOption = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
 
 mongoose.connect(mongo_url).catch(() => console.log('Unable to connect.'));
 
@@ -49,8 +46,8 @@ app.use(cors({
   credenials: true
 }))
 
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
-
 app.use(express.json({limit: '10mb'}))
 
 // Add Routes
@@ -58,7 +55,7 @@ app.use('/', user)
 app.use('/city', city)
 app.use('/country', country)
 app.use('/likes', like)
-app.use('/message', message)
+app.use('/messages', message)
 app.use('/reservations', reservation)
 app.use('/review', review)
 
